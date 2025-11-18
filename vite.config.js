a6f70ffad1c8f7f0a0e3364754cb7d2779eaca62
Amrender kumar <amrenderk66@gmail.com>
2025-11-18 09:23:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), 
-    tailwindcss()],
-     resolve: {
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+
+  base: '/jiplay/', 
+
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-        base: '/jiplay/',
   },
 })
